bb925e25ff87e4aa2653cf8719329058ea37de1a
Newiihuhu <Ninewii_huhu@hotmail.com>
2024-04-26 20:20:21 +0700

    feat: :zap: Add splash video to splash screen

--- automation_appium/lib/splash/splash_screen.dart
+++ automation_appium/lib/splash/splash_screen.dart
@@ -4,6 +4,7 @@ import 'package:automation_appium/home/page_home.dart';
 import 'package:automation_appium/login/login_screen.dart';
 import 'package:automation_appium/utils/preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 import '../onboard/onboarding_screen.dart';
 
@@ -15,22 +16,40 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late VideoPlayerController _controller;
   @override
   void initState() {
     super.initState();
+    _controller = VideoPlayerController.asset('assets/Newii_31-01.mp4');
+    _controller.initialize().then((_) => setState(() {}));
+    _controller.play();
     Timer(const Duration(seconds: 2), () {
       navigateFromSplash();
     });
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-          child: Text(
-        "Newii",
-        style: TextStyle(fontSize: 20, fontFamily: "Exo2SemiBold"),
-      )),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 60),
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
