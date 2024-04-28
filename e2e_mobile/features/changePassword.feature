dfe3ba51ee0486e6cdb758e9ec25940736dba924
Aoffy19 <aof@odds.team>
2024-04-28 15:24:25 +0700

    Update change password test case

--- e2e_mobile/features/changePassword.feature
+++ e2e_mobile/features/changePassword.feature
@@ -8,6 +8,8 @@ Feature: change password
     Then I should see dashboard screen
 
     When Click on setting button and change password
+    Then I should see the setting screen
+    When I click change password button
     Then I should see the change password screen
     When I enter the old password "<oldPassword>" and new password "<newPassword>"
     Then Click on the  change password button
--- e2e_mobile/features/step-definitions/change_password_step.ts
+++ e2e_mobile/features/step-definitions/change_password_step.ts
@@ -1,16 +1,37 @@
 import { Then, When } from "@wdio/cucumber-framework";
 
 When("Click on setting button and change password",async () => {
-    
+    const settingButton = await $('~Setting\nTab 3 of 3');
+
+    await settingButton.click();
 })
-Then('I should see the change password screen', () => {
-  // Write code here that turns the phrase above into concrete actions
+
+Then('I should see the setting screen', async() => {
+    const settingHeader = await $("~Settings");
+
+    await expect(settingHeader).toBeExisting();
+  })
+
+When('I click change password button', async() => {
+    const changePasswordButton = await $("~Change Password");
+
+    await changePasswordButton.click();
+  })
+
+Then('I should see the change password screen', async() => {
+   const changePasswordHeader = await $(`//android.view.View[@content-desc="Change Password"]`);
+
+   await expect(changePasswordHeader).toBeExisting();
 })
 
 Then('Click on the  change password button', () => {
-  // Write code here that turns the phrase above into concrete actions
+    
 })
 
 When('I enter the old password {string} and new password {string}', (s: string, s2: string) => {
   // Write code here that turns the phrase above into concrete actions
 })
+
+
+
+
