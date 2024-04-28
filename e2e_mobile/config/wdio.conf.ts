0bafa68c2182fac9cf261a7a22c8a88c5e1e2370
Miwtoo <miwyugioh@gmail.com>
2024-04-28 12:53:55 +0700

    feat(e2e): :sparkles: any step file

--- e2e_mobile/config/wdio.conf.ts
+++ e2e_mobile/config/wdio.conf.ts
@@ -141,8 +141,9 @@ export const config: Options.Testrunner = {
 
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
+        // ['../features/**/*.feature']
         // <string[]> (file/dir) require files before executing features
-        require: ['./features/step-definitions/steps.ts'],
+        require: ['./features/**/step-definitions/*.ts'],
         // <boolean> show full backtrace for errors
         backtrace: false,
         // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
