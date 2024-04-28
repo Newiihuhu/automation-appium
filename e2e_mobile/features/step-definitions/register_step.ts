d889baa9daaafa8b2111eaa14502b1897fce3709
TarTheesaran <tar.theesaran@odds.team>
2024-04-28 14:30:13 +0700

    feat: update scenario fill register form(WIP)

--- e2e_mobile/features/step-definitions/register_step.ts
+++ e2e_mobile/features/step-definitions/register_step.ts
@@ -12,4 +12,22 @@ Then('I should see register screen', async () => {
     const registerHeader = await $("~Register");
     await expect(registerHeader).toBeExisting();
 })
+
+// Given('I am on the register screen', async () => {
+//     const registerHeader = await $("~Register");
+
+//     await expect(registerHeader).toBeExisting();
+// })
+
+// When('I fill all fields with username {string} email {string} and password {string}', async (s: string, s2: string, s3: string) => {
+
+//     const userNameTextBox = await $("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]");
+//     const emailTextBox = await $("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]");
+//     const passwordTextBox = await $("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[3]");
+//     const confirmPasswordTextBox = await $("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[4]");
+
+// await userNameTextBox.click();
+// await userNameTextBox.setValue("testtest");
+// await emailTextBox.click();
+// })
   
